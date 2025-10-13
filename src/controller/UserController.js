3c6263c2a07cb1886a6f8ab63d43954d7ac7a8eb
Deepesh <navanideepesh67@gmail.com>
2025-10-13 22:35:49 +0530

    fetching name,id,email by login

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -72,7 +72,14 @@ const login = async (req, res) => {
       return res.status(400).json({ message: "Incorrect password!" });
     }
 
-    return res.status(200).json({ message: "Login successful!" });
+    return res
+      .status(200)
+      .json({
+        message: "Login successful!",
+        name: user.name,
+        email: user.email,
+        id: user.id,
+      });
   } catch (error) {
     console.log(error.message);
     return res
